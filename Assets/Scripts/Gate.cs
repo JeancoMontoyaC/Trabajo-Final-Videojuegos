5c737fe551be35fa63a2ce3bfaf74ae82d4aef8b
Juan David Henao Marín <judhenaoma@unal.edu.co>
2023-06-21 14:58:30 -0500

    fixing gate activation bug

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public static bool gateIsActive;
+    public static bool buttonRightPressed;
+    public static bool buttonLeftPressed;
+}

c5b12839e42e591bd2a239f8af56cc6302252c7d
Juan David Henao Marín <judhenaoma@unal.edu.co>
2023-06-22 14:30:10 -0500

    level 3 ready

--- Assets/Scripts/FBObstaclesManager.cs
+++ Assets/Scripts/FBObstaclesManager.cs
@@ -5,9 +5,23 @@ public class FBObstaclesManager : MonoBehaviour
 {
     private bool isDead;
 
+    // When the element that triggers the death is a collider
     public void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Water") || col.CompareTag("Toxic"))
+        {
+            GlobalGameManager.Instance.showDeathMenu();
+            int totalDeaths = PlayerPrefs.GetInt("Level-deaths", 0) + 1;
+            PlayerPrefs.SetInt("Level-deaths", totalDeaths);
+            Destroy(gameObject);
+            isDead = true;
+        }
+    }
+    
+    // When the element that triggers the death is a rigidBody
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Slime"))
         {
             GlobalGameManager.Instance.showDeathMenu();
             int totalDeaths = PlayerPrefs.GetInt("Level-deaths", 0) + 1;
--- Assets/Scripts/WGCollectibleInteraction.cs
+++ Assets/Scripts/WGCollectibleInteraction.cs
@@ -25,7 +25,7 @@ public class WGCollectibleInteraction : MonoBehaviour
                 }
                 else
                 {
-                    
+                    GameManager3.instance.collectingBlueDiamonds();   
                 }
             }
         }
